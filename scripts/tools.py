1e1e1d84c7138979cb2951d0b4c59e2285405af9
emlbrg <emilia.bergoglio@gmail.com>
2024-10-22 13:18:59 +0900

    modified:   data/rosalind_fibd.txt

--- scripts/tools.py
+++ scripts/tools.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 def parse_fasta(fasta_file_path: str) -> Dict:
     """
@@ -22,6 +22,34 @@ def parse_fasta(fasta_file_path: str) -> Dict:
                 sequences[current_sequence] += line
     return sequences
 
+def parse_fasta_list(fasta_file_path: str) -> List:
+    """
+    Parse a FASTA file and return a list of sequences.
+
+    Args:
+        file_path (str): Path to the FASTA file.
+
+    Returns:
+        list: A list of DNA sequences.
+    """
+    with open(fasta_file_path, 'r') as file:
+        sequences = []
+        current_seq = ''
+        
+        for line in file:
+            line = line.strip()
+            if line.startswith('>'):  # Skip the header line
+                if current_seq:  # Save the previous sequence
+                    sequences.append(current_seq)
+                    current_seq = ''
+            else:
+                current_seq += line  # Append the line to the current sequence
+
+        if current_seq:  # Append the last sequence
+            sequences.append(current_seq)
+    
+    return sequences
+
 def parse_fasta_str(fasta_file_path: str) -> str:
     """
     Parse the sequence from a FASTA formatted file.
@@ -35,5 +63,5 @@ def parse_fasta_str(fasta_file_path: str) -> str:
     with open(fasta_file_path, 'r') as file:
         lines = file.readlines()
         # Remove the first line (header) and concatenate the rest
-        rna_sequence = ''.join(line.strip() for line in lines if not line.startswith('>'))
-    return rna_sequence
+        sequence = ''.join(line.strip() for line in lines if not line.startswith('>'))
+    return sequence
